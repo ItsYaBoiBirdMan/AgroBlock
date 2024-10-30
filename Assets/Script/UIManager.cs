8bad257bb305fe358188659403d45067b75b5985
ItsYaBoiBirdMan <94912025+ItsYaBoiBirdMan@users.noreply.github.com>
2024-10-30 21:30:32 +0000

    Started work on the ui

---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private GameObject BlockScreen;
+
+    [SerializeField] private GameObject SpecificBlockScreen;
+
+    public void test()
+    {
+        
+    }
+}
