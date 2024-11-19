4710ee8c1dd8783c4b3d2730b723359bc7fdba55
ItsYaBoiBirdMan <94912025+ItsYaBoiBirdMan@users.noreply.github.com>
2024-11-19 20:27:28 +0000

    Commiting for merge

--- Assets/FSM/FSMActions.cs
+++ Assets/FSM/FSMActions.cs
@@ -6,31 +6,59 @@ using UnityEngine;
 public class FSMActions : MonoBehaviour
 {
     [SerializeField] private Animator FSM;
-    [SerializeField] private float waterLevel;
 
     private void WaterStateHandler()
     {
         switch (FSM.GetFloat("Water Level"))
         {
-            case > 60:
+            case > 61:
                  Debug.Log("Too much Water");
-                 FSM.SetFloat("Water Level", 55);
-                //Debug.Log("Water Level: " + FSM.GetFloat("Water Level"));
+                 //FSM.SetFloat("Water Level", 55);
+                 //Debug.Log("Water Level: " + FSM.GetFloat("Water Level"));
                 break;
             case < 50:
                 Debug.Log("Low Water");
-                FSM.SetFloat("Water Level", 55);
+                //FSM.SetFloat("Water Level", 55);
                 //Debug.Log("Water Level: " + FSM.GetFloat("Water Level"));
                 break;
             default:
-                Debug.Log("Water Normal");
+                break;  
+        }
+    }
+
+    private void TemperatureStateHandler()
+    {
+        switch (FSM.GetFloat("Temperature"))
+        {
+            case > 31:
+                Debug.Log("Temperature too high");
+                break;
+            case < 20:
+                Debug.Log("Temperature too low");
+                break;
+            default:
+                break;
+        }
+    }
+    
+    private void LightStateHandler()
+    {
+        switch (FSM.GetFloat("Light Level"))
+        {
+            case > 91:
+                Debug.Log("Light too bright");
+                break;
+            case < 70:
+                Debug.Log("Light too dim");
+                break;
+            default:
                 break;
         }
     }
 
     private void Update()
     {
-        FSM.SetFloat("Water Level", waterLevel);
+       
     }
 }
 
