0d9cb30c402cf87fa99b89ed8339dfc8ba55e65a
ItsYaBoiBirdMan <94912025+ItsYaBoiBirdMan@users.noreply.github.com>
2024-12-16 16:23:49 +0000

    Leaderboard logic done

--- Assets/Script/Gamification/LeaderboardManager.cs
+++ Assets/Script/Gamification/LeaderboardManager.cs
@@ -1,9 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
 public class LeaderboardManager : MonoBehaviour
 {
-    [SerializeField] private List<int> Scores;
+    [SerializeField] private List<GameObject> EntryList;
+    [SerializeField] private List<UserDataManager> UsersList;
+    [SerializeField] private UserDataManager userData;
     
+    private void SetLeaderboardEntry(GameObject entry ,int pos, string name, int points)
+    {
+        entry.transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>().text = pos.ToString();
+        entry.transform.GetChild(1).GetChild(0).GetComponent<TextMeshProUGUI>().text = name;
+        entry.transform.GetChild(2).GetChild(0).GetComponent<TextMeshProUGUI>().text = points.ToString();
+    }
+
+    private void SortLeaderboard()
+    {
+        UsersList = UsersList.OrderByDescending(obj => obj.GetTotalPoints()).ToList();
+        for (int i = 0; i < 9; i++)
+        {
+            SetLeaderboardEntry(EntryList[i], i + 1, UsersList[i].GetName(), UsersList[i].GetTotalPoints());
+        }
+    }
+
+    private void Start()
+    {
+        SortLeaderboard();
+    }
 }
